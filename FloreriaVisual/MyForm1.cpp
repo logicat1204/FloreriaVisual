9450a8def598fada90042a2cb507cd965e96bde2
Alvaro David Arancibia Estrada <aarancibia142@gmail.com>
2025-06-08 10:06:52 -0400

    Agregar archivos de proyecto.

---
+++ FloreriaVisual/MyForm1.cpp
@@ -0,0 +1,11 @@
+#include "MyForm1.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+[STAThreadAttribute]
+void Main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	FloreriaVisual::MyForm1 form;
+	Application::Run(% form);
+}
